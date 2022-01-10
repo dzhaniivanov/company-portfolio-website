e4fd79d06992fab1d780c10014eed444de3bd707
dzhani ivanov <djani.ivanov94@gmail.com>
2022-01-10 15:42:43 +0200

    working on navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
+import Contact from "./components/Contact/Contact";
+import Topbar from "./components/Topbar/Topbar";
+import Portfolio from "./components/Portfolio/Portfolio";
+import Works from "./components/Works/Works";
+import Intro from "./components/Intro/Intro";
+import "./App.scss"
+import { useState } from "react";
+
+
 
 
 function App() {
+
+  const [menuOpen, setMenuOpen] = useState(false);
+
+
   return (
-    <div className="App">
-    dsdsd   
+    <div className="app">
+      <Topbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+      <div className="sections">
+        <Intro />
+        <Portfolio />
+        <Works />
+        <Contact />
+      </div>
     </div>
   );
 }
---
+++ src/components/Topbar/Topbar.js
@@ -0,0 +1,26 @@
+import "./Topbar.scss";
+
+
+const Topbar = ({ menuOpen, setMenuOpen }) => {
+    return (
+        <div className={"topbar " + (menuOpen && "active")}>
+            <div className="wrapper">
+                <div className="left">
+                    <a href="#intro" className="logo">
+                        oil food technologies.
+                    </a>
+                </div>
+                <div className="right">
+                    <div className="hamburger" onClick={() => setMenuOpen(!menuOpen)}>
+                        <span className="line1"></span>
+                        <span className="line2"></span>
+                        <span className="line3"></span>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Topbar
